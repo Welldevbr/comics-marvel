f24b54660bd262c002f5e93233ecb5511c0b64ff
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2023-01-21 00:18:39 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+export function App() {
+
+  const baseURL = 'http://gateway.marvel.com/v1/public'
+
+  const publicKey = '9690bc81233bab5e02074d49b1dae854'
+  const privateKey = '6e70af4d7060da193bcef80dd7e3a90f7f228219'
+
+  return (
+    <div>
+      Marvel
+    </div>
+  )
+}
