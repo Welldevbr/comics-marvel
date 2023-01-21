e63bba34127dc448657798ad842d59b28b86ddc8
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2023-01-21 15:25:56 -0300

    Testing API

--- src/hooks/useAPI.ts
+++ src/hooks/useAPI.ts
@@ -3,6 +3,7 @@ import { useEffect } from 'react'
 import axios from 'axios'
 import md5 from 'md5'
 
+export function useAPI(){
   const publicKey = '9690bc81233bab5e02074d49b1dae854'
   const privateKey = '6e70af4d7060da193bcef80dd7e3a90f7f228219'
 
@@ -10,18 +11,9 @@ import md5 from 'md5'
 
   const hash = md5(time + privateKey + publicKey)
 
-export function App() {
   useEffect(() => {
     axios.get(
-      `http://gateway.marvel.com/v1/public/characters?ts=${time}&apikey=${publicKey}&hash=${hash}`
+      `http://gateway.marvel.com/v1/public/comics?ts=${time}&apikey=${hash}&hash=${hash}`
     )
-    .then(response => console.log(response))
-    .catch(err => console.log(err))
   }, [])
-
-  return (
-    <div>
-      Marvel
-    </div>
-  )
-} 
+}
