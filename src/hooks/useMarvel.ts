5daafcfebe151bc7a765ff32832ed00009dca7cf
Wellington Santana <90020289+Welldevbr@users.noreply.github.com>
2023-01-26 17:25:39 -0300

    Mudanças a serem submetidas: 	new file:   .env.local.exemple 	modified:   package-lock.json 	modified:   package.json 	deleted:    src/.env.local.exemple 	modified:   src/hooks/useMarvel.ts

--- src/hooks/useMarvel.ts
+++ src/hooks/useMarvel.ts
@@ -3,8 +3,8 @@ import { Comic } from "../interfaces/GeneralTypes";
 import axios from "axios";
 import md5 from "md5";
 
-const apiKey = "9690bc81233bab5e02074d49b1dae854"
-const privateKey = "6e70af4d7060da193bcef80dd7e3a90f7f228219"
+const apiKey = import.meta.env.VITE_API_KEY;
+const privateKey = import.meta.env.VITE_PRIVATE_KEY
 
   const getHash = () => {
     let ts = Date.now();
